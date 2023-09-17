23b7afe4fdeb3446a6e859d55d35651f45a36198
Sameer <sameer.q08@gmail.com>
2023-09-17 21:31:05 +0530

    "Api"

--- lib/apis/apis.dart
+++ lib/apis/apis.dart
@@ -1,10 +1,39 @@
 import 'dart:developer';
 
+import 'package:csv/csv.dart';
 import 'package:http/http.dart';
 
+import '../helpers/my_dialogs.dart';
+import '../models/vpn.dart';
+
 class APIs {
-  static Future<void> getVPNServers() async {
-    final res = await get(Uri.parse('http://www.vpngate.net/api/iphone'));
-    log(res.body);
+  static Future<List<Vpn>> getVPNServers() async {
+    final List<Vpn> vpnList = [];
+
+    try {
+      final res = await get(Uri.parse('http://www.vpngate.net/api/iphone/'));
+      final csvString = res.body.split("#")[1].replaceAll('*', '');
+
+      List<List<dynamic>> list = const CsvToListConverter().convert(csvString);
+
+      final header = list[0];
+
+      for (int i = 1; i < list.length - 1; ++i) {
+        Map<String, dynamic> tempJson = {};
+
+        for (int j = 0; j < header.length; ++j) {
+          tempJson.addAll({header[j].toString(): list[i][j]});
+        }
+        vpnList.add(Vpn.fromJson(tempJson));
+      }
+    } catch (e) {
+      MyDialogs.error(msg: e.toString());
+      log('\ngetVPNServersE: $e');
+    }
+    vpnList.shuffle();
+
+    //if (vpnList.isNotEmpty) //Pref.vpnList = vpnList;
+
+    return vpnList;
   }
 }
--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
 
-import '../apis/apis.dart';
+import '../apis/controller/location_controller.dart';
+
+late Size mq;
 
 class LocationScreen extends StatefulWidget {
   const LocationScreen({super.key});
@@ -10,18 +14,62 @@ class LocationScreen extends StatefulWidget {
 }
 
 class _LocationScreenState extends State<LocationScreen> {
+  final _controller = LocationController();
   @override
   void initState() {
     super.initState();
-    APIs.getVPNServers();
+    _controller.getVpnData();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    mq = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text("Secure Proxy VPN"),
       ),
+      body: Obx(() => _controller.isLoading.value
+          ? _loadingWidget()
+          : _controller.vpnList.isEmpty
+              ? _noVPNFound()
+              : _vpnData()),
     );
   }
+
+  _vpnData() => ListView.builder(
+        itemCount: _controller.vpnList.length,
+        itemBuilder: (ctx, i) => Text(_controller.vpnList[i].hostname),
+      );
+
+  _loadingWidget() => SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            LottieBuilder.asset('assets/lottie/loading.json',
+                width: mq.width * .7),
+            Text(
+              'Loading VPN...',
+              style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black54,
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      );
+  _noVPNFound() => Center(
+        child: Text(
+          'No VPN Found...',
+          style: TextStyle(
+              fontSize: 18, color: Colors.black54, fontWeight: FontWeight.bold),
+        ),
+      );
 }
